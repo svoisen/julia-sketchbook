3f95159091e3e38bd9e1f7adaec186cdd2dd9bbf
Sean Voisen <sean@voisen.org>
2025-03-21 14:11:30 -0700

    add: basic asemic character sketch using catmull rom spline

---
+++ asemic_character01.jl
@@ -0,0 +1,31 @@
+include("splines.jl")
+
+using Luxor
+import ..Splines as S
+
+width = 400.0
+height = 400.0
+
+points = [Point(rand(-width/2:width/2), rand(-height/2:height/2)) for _ in 1:5]
+extended_points = [
+    points[1] - (points[2] - points[1]),
+    points...,
+    points[end] + (points[end] - points[end-1])
+]
+
+Drawing(400, 400)
+origin()
+background("white")
+
+sethue("red")
+for (i, point) in enumerate(extended_points)
+    circle(point, 5, :fill)
+    text(string(i), point + Point(10, 5))
+end
+
+setline(2)
+sethue("blue")
+S.catmullromspline(extended_points, 20)
+
+finish()
+preview()
