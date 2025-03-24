ed92695cb097f378c933a7f8dc4df2da926cd359
Sean Voisen <sean@voisen.org>
2025-03-24 16:47:22 -0700

    vary linear and spline graph traversal

--- asemic_glyph02.jl
+++ asemic_glyph02.jl
@@ -27,7 +27,6 @@ Base.show(io::IO, e::Edge) = print(io, "Edge($(e.v1), $(e.v2))")
 # We will use "point" to refer to a point in Luxor.
 pt2vert(p) = (p.x, p.y)
 vert2pt(v) = Point(v[1], v[2])
-edge2pts(e) = (vert2pt(e[1]), vert2pt(e[2]))
 
 """
     edgelength(e::Edge)
@@ -87,25 +86,32 @@ end
     traverse_graph(graph_edges, endpoints)
 
 Given a set of graph edges and a set of endpoints, traverse the graph starting
-from one of the endpoints.
+from one of the endpoints for n number of edges.
 """
-function traverse_graph(graph_edges)
+function traverse_graph(graph_edges, total_edges=4)
     endpoints = get_endpoints(graph_edges)
 
     if isempty(endpoints)
-        println("No endpoints found!")
-        return [], []
+        println("No endpoints found, removing edge")
+        graph_edges = graph_edges[1:end-1]
+        endpoints = get_endpoints(graph_edges)
     end
 
     current_vertex = endpoints[rand(1:length(endpoints))]
-    visited_edges = Set{Edge}()
+
+    # At the end of this, we want the order of edges and vertices visited
+    # not just the set, but the set is useful for more efficient lookup
+    visited_edges_set = Set{Edge}()
+    visited_edges = Vector{Edge}()
     visited_vertices_set = Set{Int}()
     visited_vertices = Vector{Int}()
     push!(visited_vertices_set, current_vertex)
 
-    while true
+    num_edges = 0
+
+    while num_edges < total_edges
         next_edge = nothing
-        for edge in filter(e -> ! (e in visited_edges), graph_edges)
+        for edge in filter(e -> ! (e in visited_edges_set), graph_edges)
             if edge.v1 == current_vertex || edge.v2 == current_vertex
                 next_edge = edge
                 break
@@ -118,8 +124,12 @@ function traverse_graph(graph_edges)
         end
 
         # Otherwise, add the edge to the visited edges and move to the next vertex
+        push!(visited_edges_set, next_edge)
         push!(visited_edges, next_edge)
+        num_edges += 1
+
         current_vertex = next_edge.v1 == current_vertex ? next_edge.v2 : next_edge.v1
+
         push!(visited_vertices_set, current_vertex)
         push!(visited_vertices, current_vertex)
     end
@@ -127,43 +137,99 @@ function traverse_graph(graph_edges)
     return visited_edges, visited_vertices
 end
 
-function main()
-    point_cloud = Random.distributed_pointcloud(12, 50, 20., 20., width - 40., height - 40.)
-    vertices = map(pt2vert, point_cloud)
-    graph_edges = generate_graph(vertices)
-    traversed_edges, traversed_vertices = traverse_graph(graph_edges)
-    traversed_points = map(vert2pt, map(v -> vertices[v], traversed_vertices))
+function draw_stroke_linear(stroke)
+    for i in 1:length(stroke) - 1
+        line(vert2pt(stroke[i]), vert2pt(stroke[i + 1]))
+        strokepath()
+    end
+end
+
+function draw_stroke_spline(stroke)
+    if length(stroke) < 3
+        println("Not enough vertices to draw a spline!")
+        return
+    end
+
+    points = map(vert2pt, stroke)
     extended_points = [
-        traversed_points[1] - (traversed_points[2] - traversed_points[1]),
-        traversed_points...,
-        traversed_points[end] + (traversed_points[end] - traversed_points[end-1])
+        points[1] - (points[2] - points[1]),
+        points...,
+        points[end] + (points[end] - points[end-1])
     ]
+    S.catmullromspline(extended_points, 20)
+end
+
+"""
+    draw_graph(graph_edges, vertices)
+
+Debugging function to draw the graph edges and vertices.
+"""
+function draw_graph(graph_edges, vertices)
+    sethue("black")
+    for edge in graph_edges
+        p1 = vert2pt(vertices[edge.v1])
+        p2 = vert2pt(vertices[edge.v2])
+        line(p1, p2)
+        strokepath()
+    end
+
+    sethue("red")
+    for vertex in vertices
+        circle(vert2pt(vertex), 4, :fill)
+    end
+end
+
+function draw_diacritical()
+    p = randompoint(20., 20., width - 20., height - 20.)
+    circle(p, 10)
+    strokepath()
+end
+
+function main()
+    point_cloud = Random.distributed_pointcloud(14, 50, 20., 20., width - 40., height - 40.)
+    vertex_cloud = map(pt2vert, point_cloud)
+    graph_edges = generate_graph(vertex_cloud)
+
+    remaining_edges = copy(graph_edges)
+    strokes = Vector{Vector{Int}}()
+
+    # Traversal logic:
+    # 1. Find the endpoints of the graph.
+    # 2. Start at one of the endpoints.
+    # 3. Traverse the graph by moving to the next edge that is connected to the current vertex. Perform a maximum
+    #    of 2-4 edge traversals.
+    # 4. Remove all traversed edges from the graph.
+    # 4. If there are more endpoints to visit, repeat step 2.
+    while(length(remaining_edges) > 0)
+        traversed_edges, traversed_vertices = traverse_graph(remaining_edges, rand(2:5))
+        # If we can't find any more traversals or vertices because there are no more endpoints, we're done
+        if length(traversed_edges) == 0 || length(traversed_vertices) == 0
+            break
+        end
+
+        push!(strokes, traversed_vertices)
+        remaining_edges = setdiff(remaining_edges, traversed_edges)
+    end
+
+    vertex_strokes = map(stroke -> map(i -> vertex_cloud[i], stroke), strokes)
 
     Drawing(width, height)
     background("white")
     setline(1)
+
+    draw_graph(graph_edges, vertex_cloud)
+
     sethue("blue")
-    S.catmullromspline(extended_points, 20)
 
-    # for edge in graph_edges
-    #     if edge in traversed_edges
-    #         sethue("red")
-    #     else
-    #         sethue("black")
-    #     end
-    #     v1 = vertices[edge.v1]
-    #     v2 = vertices[edge.v2]
-    #     line(vert2pt(v1), vert2pt(v2))
-    #     strokepath()
-    #     circle(vert2pt(v1), 3, :fill)
-    #     text(string(edge.v1), vert2pt(v1) + Point(10, 5))
-    # end
+    for stroke in vertex_strokes
+        if rand() > 0.25
+            draw_stroke_spline(stroke)
+        else
+            draw_stroke_linear(stroke)
+        end
+    end
 
-    # for endpoint in endpoints
-    #     sethue("red")
-    #     circle(vert2pt(vertices[endpoint]), 3, :fill)
-    #     text(string(endpoint), vert2pt(vertices[endpoint]) + Point(10, 5))
-    # end
+    draw_diacritical()
 
     finish()
     preview()
