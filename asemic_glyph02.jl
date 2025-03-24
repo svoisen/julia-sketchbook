0629a483e941f61339132a3d84b12baa49053024
Sean Voisen <sean@voisen.org>
2025-03-24 14:36:31 -0700

    finish travsering graph in asemic_glyph02

--- asemic_glyph02.jl
+++ asemic_glyph02.jl
@@ -3,6 +3,7 @@ include("random.jl")
 
 using Luxor, DelaunayTriangulation
 import .Random as R
+import .Splines as S
 
 width = 400.0
 height = 400.0
@@ -28,10 +29,6 @@ pt2vert(p) = (p.x, p.y)
 vert2pt(v) = Point(v[1], v[2])
 edge2pts(e) = (vert2pt(e[1]), vert2pt(e[2]))
 
-points = Random.distributed_pointcloud(8, 50, 20., 20., width - 40., height - 40.)
-vertices = map(pt2vert, points)
-triangulation = DelaunayTriangulation.triangulate(vertices)
-
 """
     edgelength(e::Edge)
 
@@ -45,13 +42,11 @@ function edgelength(e::Edge, vertices::Vector)
 end
 
 """
-    generate_graph(points)
 
 Given a set of points, generate a graph by connecting the points with edges
 that are not the longest edge of a triangle in the Delaunay triangulation.
 """
-function generate_graph(points)
-    vertices = map(pt2vert, points)
+function generate_graph(vertices)
     triangulation = DelaunayTriangulation.triangulate(vertices)
     longest_edges = Vector{Edge}()
     short_edges = Vector{Edge}() 
@@ -88,17 +83,29 @@ function get_endpoints(graph_edges)
     return endpoints
 end
 
-function traverse_graph(graph_edges, endpoints)
-    # Start at an endpoint
+"""
+    traverse_graph(graph_edges, endpoints)
+
+Given a set of graph edges and a set of endpoints, traverse the graph starting
+from one of the endpoints.
+"""
+function traverse_graph(graph_edges)
+    endpoints = get_endpoints(graph_edges)
+
+    if isempty(endpoints)
+        println("No endpoints found!")
+        return [], []
+    end
+
     current_vertex = endpoints[rand(1:length(endpoints))]
     visited_edges = Set{Edge}()
-    visited_vertices = Set{Int}()
-    push!(visited_vertices, current_vertex)
+    visited_vertices_set = Set{Int}()
+    visited_vertices = Vector{Int}()
+    push!(visited_vertices_set, current_vertex)
 
     while true
-        # Find the next edge
         next_edge = nothing
-        for edge in graph_edges
+        for edge in filter(e -> ! (e in visited_edges), graph_edges)
             if edge.v1 == current_vertex || edge.v2 == current_vertex
                 next_edge = edge
                 break
@@ -110,18 +117,10 @@ function traverse_graph(graph_edges, endpoints)
             break
         end
 
-        # If we've already visited the edge, we're done
-        if next_edge in visited_edges
-            break
-        end
-
         # Otherwise, add the edge to the visited edges and move to the next vertex
         push!(visited_edges, next_edge)
-        if next_edge.v1 == current_vertex
-            current_vertex = next_edge.v2
-        else
-            current_vertex = next_edge.v1
-        end
+        current_vertex = next_edge.v1 == current_vertex ? next_edge.v2 : next_edge.v1
+        push!(visited_vertices_set, current_vertex)
         push!(visited_vertices, current_vertex)
     end
 
@@ -129,28 +128,42 @@ function traverse_graph(graph_edges, endpoints)
 end
 
 function main()
+    point_cloud = Random.distributed_pointcloud(12, 50, 20., 20., width - 40., height - 40.)
+    vertices = map(pt2vert, point_cloud)
+    graph_edges = generate_graph(vertices)
+    traversed_edges, traversed_vertices = traverse_graph(graph_edges)
+    traversed_points = map(vert2pt, map(v -> vertices[v], traversed_vertices))
+    extended_points = [
+        traversed_points[1] - (traversed_points[2] - traversed_points[1]),
+        traversed_points...,
+        traversed_points[end] + (traversed_points[end] - traversed_points[end-1])
+    ]
+
     Drawing(width, height)
     background("white")
     setline(1)
+    sethue("blue")
+    S.catmullromspline(extended_points, 20)
 
-    graph_edges = generate_graph(points)
-    endpoints = get_endpoints(graph_edges)
-    traversal_edges, traversal_vertices = traverse_graph(graph_edges, endpoints)
-    println(traversal_vertices)
+    # for edge in graph_edges
+    #     if edge in traversed_edges
+    #         sethue("red")
+    #     else
+    #         sethue("black")
+    #     end
+    #     v1 = vertices[edge.v1]
+    #     v2 = vertices[edge.v2]
+    #     line(vert2pt(v1), vert2pt(v2))
+    #     strokepath()
+    #     circle(vert2pt(v1), 3, :fill)
+    #     text(string(edge.v1), vert2pt(v1) + Point(10, 5))
+    # end
 
-    for edge in graph_edges
-        sethue("black")
-        v1 = vertices[edge.v1]
-        v2 = vertices[edge.v2]
-        line(vert2pt(v1), vert2pt(v2))
-        strokepath()
-    end
-
-    for endpoint in endpoints
-        sethue("red")
-        circle(vert2pt(vertices[endpoint]), 3, :fill)
-        text(string(endpoint), vert2pt(vertices[endpoint]) + Point(10, 5))
-    end
+    # for endpoint in endpoints
+    #     sethue("red")
+    #     circle(vert2pt(vertices[endpoint]), 3, :fill)
+    #     text(string(endpoint), vert2pt(vertices[endpoint]) + Point(10, 5))
+    # end
 
     finish()
     preview()
