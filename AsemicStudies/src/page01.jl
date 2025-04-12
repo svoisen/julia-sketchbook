9746f5fd7a1469fcbd8dd4c58882b3a91a2a5370
Sean Voisen <sean@voisen.org>
2025-04-12 10:26:53 -0700

    update asemic page sketch

--- AsemicStudies/src/page01.jl
+++ AsemicStudies/src/page01.jl
@@ -10,6 +10,11 @@ struct Glyph
     width::Real
 end
 
+pagewidth = 8inch
+pageheight = 10inch
+margin = 0.5inch
+lineheight = 0.75inch
+
 """
     generate_glyph_pts(origin::Point, width::Real, minheight::Real, maxheight::Real, shouldpolyfit::Bool = true)
 
@@ -29,7 +34,7 @@ function generate_glyph_pts(origin::Point, width::Real, minheight::Real, maxheig
 end
 
 function generate_glyph(origin::Point, width::Real, minheight::Real, maxheight::Real)
-    pts = generate_glyph_pts(origin, width, minheight, maxheight)
+    pts = generate_glyph_pts(origin, width, minheight, maxheight, true)
     return Glyph(pts, width)
 end
 
@@ -53,10 +58,18 @@ end
 function renderword(word::Vector{Char}, glyphmap::Dict{Char, Glyph}, position::Point)
     wordpts = Vector{Point}()
     glyphs = map(c -> glyphmap[c], word)
-    totalwidth = sum(glyph.width for glyph in glyphs)
+    twoglyph_width = sum(glyph.width for glyph in glyphs[1:2])
+    xpos, ypos = position.x, position.y
 
-    xpos = position.x + totalwidth > 7.5inch ? 0.5inch : position.x
-    ypos = xpos == position.x ? position.y : position.y + 0.5inch
+    if xpos + twoglyph_width > 7.5inch
+        xpos = 0.5inch
+        ypos += lineheight
+    else
+        # Truncate the word to fit within the page width
+        while length(glyphs) > 2 && xpos + sum(glyph.width for glyph in glyphs) > 7.5inch
+            glyphs = glyphs[1:end-1]
+        end
+    end
 
     for glyph in glyphs
         pos = Point(xpos, ypos)
@@ -68,17 +81,14 @@ function renderword(word::Vector{Char}, glyphmap::Dict{Char, Glyph}, position::P
     return Point(xpos, ypos)
 end
 
-pagewidth = 8inch
-pageheight = 10inch
-
-@draw begin
-    setline(0.5)
+@svg begin
+    setline(1.5)
     sethue("black")
 
     glyphmap = generate_glyph_map(0.5inch, 0.25inch, 0.75inch)
-    words = [generate_word(rand(3:8)) for _ in 1:10]
+    words = [generate_word(rand(3:8)) for _ in 1:30]
     origin(0, 0)
-    pos = Point(0.5inch, 1inch)
+    pos = Point(margin, margin)
 
     for word in words
         global pos = renderword(word, glyphmap, pos)
